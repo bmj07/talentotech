a8852e9558cc1c7720ba7c2fc26ea49888237fdd
bmj07 <melryjhulyyb@gmail.com>
2024-12-06 22:30:59 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const display = document.getElementById('display');
+    const buttons = Array.from(document.getElementsByClassName('btn'));
+
+    let currentInput = '';
+    let operator = '';
+    let previousInput = '';
+
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const value = e.target.innerText;
+
+            if (value === 'C') {
+                currentInput = '';
+                operator = '';
+                previousInput = '';
+                display.innerText = '0';
+            } else if (value === '=') {
+                if (currentInput && previousInput && operator) {
+                    currentInput = eval(`${previousInput}${operator}${currentInput}`);
+                    display.innerText = currentInput;
+                    operator = '';
+                    previousInput = '';
+                }
+            } else if (['+', '-', '*', '/'].includes(value)) {
+                if (currentInput) {
+                    previousInput = currentInput;
+                    operator = value;
+                    currentInput = '';
+                }
+            } else {
+                currentInput += value;
+                display.innerText = currentInput;
+            }
+        });
+    });
+});
