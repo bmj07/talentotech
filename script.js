b6d00e4338d51226528d69fd81a35425c6b86842
bmj07 <melryjhulyyb@gmail.com>
2024-12-06 23:03:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const display = document.getElementById('display');
     const buttons = Array.from(document.getElementsByClassName('btn'));
